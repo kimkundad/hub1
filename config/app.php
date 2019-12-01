740db7ec89416ed8336ba2c0461c8fb2e647485b
kimkundad <kim.kundad@gmail.com>
2019-12-01 19:12:35 +0700

    new server

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        Milon\Barcode\BarcodeServiceProvider::class,
 
 
         /*
@@ -231,8 +230,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
-        'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
 
     ],
 
